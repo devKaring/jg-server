0f2be20d91dd6e232bf82529bbd6c88d4e4251e3
jgnet97 <jgnet97@naver.com>
2026-03-31 21:55:13 +0900

    DB 연동

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express, { Request, Response } from 'express';
 import userRoutes from './routes/userRoutes'
+
+import pool from './config/db';
+
 
 const app = express();
 const PORT = 3000;
@@ -10,8 +16,21 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.get('/api/health', (req, res) => {
-    res.json({ status: 'ok' });
+
+app.get('/api/health', async (req: Request, res: Response) => {
+  try {
+    const [rows] = await pool.query('SELECT 1 AS result');
+    res.json({
+      status: 'ok',
+      db: rows,
+    });
+  } catch (error) {
+    console.error('DB 연결 오류:', error);
+    res.status(500).json({
+      status: 'error',
+      message: 'DB 연결 실패',
+    });
+  }
 });
 
 app.use('/api/users', userRoutes)
